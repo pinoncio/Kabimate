2d2c2b835ce39eff2b13bd6508ea39f5a6fb014b
NicolasPavz <npavez43@gmail.com>
2024-10-17 21:50:24 -0300

    usuario controlador

--- Back/src/routes/usuarioRoutes.ts
+++ Back/src/routes/usuarioRoutes.ts
@@ -3,9 +3,10 @@ import { newUsuario, desactivarUsuario, activarUsuario, updateUsuario, loginUser
 
 const router = Router();
 
-router.post('/', newUsuario);
-router.put('/desactivar/:id_usuario', desactivarUsuario);
-router.put('/activar/:id_usuario', activarUsuario);
-router.put('/update/:id_usuario', updateUsuario);
-router.post('/login', loginUser);
+router.post('/', newUsuario as any);
+router.put('/desactivar/:id_usuario', desactivarUsuario as any);
+router.put('/activar/:id_usuario', activarUsuario as any);
+router.put('/update/:id_usuario', updateUsuario as any);
+router.post('/login', loginUser as any);
+
 export default router;
